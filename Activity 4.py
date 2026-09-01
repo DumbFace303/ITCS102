e7c2c11eda439f3878e0c44b98fc425914379de5
DumbFace303 <markbabasa131@gmail.com>
2026-09-01 12:18:13 +0800

    Add files via upload

---
+++ Activity 4.py
@@ -0,0 +1,16 @@
+# escape sequence
+# \n
+# \t
+# \r
+# \\
+# \*
+
+
+
+
+
+
+
+motto2 = "\t\t\tThe Quick Brown Fox Jumps over the Lazy Dog. \"A negative mind would never give you a positive life\""
+
+print(motto2)
