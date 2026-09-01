88b2a4a91e9d13db80f59f119fbdaac085c77e9f
DumbFace303 <markbabasa131@gmail.com>
2026-09-01 12:35:21 +0800

    Add files via upload

---
+++ activity 6.py
@@ -0,0 +1,2 @@
+name = input("What is your name? -")
+print("Hi my name is",name,", how are you doing today?")
---
+++ code_challenge2.py
@@ -0,0 +1,12 @@
+money = 1500
+print("Money =", money)
+
+print("1000 =", money // 1000)
+print("500 =", (money % 1000) // 500)
+print("200 =", (money % 500) // 200)
+print("100 =", (money % 200) // 100)
+print("50 =", (money % 100) // 50)
+print("20 =", (money % 50) // 20)
+print("10 =", (money % 20) // 10)
+print("5 =", (money % 10) // 5)
+print("1 =", (money % 5) // 1)
