127ce4c30b2375ad63c117117c7641934a7f33ab
DumbFace303 <markbabasa131@gmail.com>
2026-09-03 12:26:47 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,12 @@
+hobbies = ""
+
+h = input("What are your hobbies? -->")
+hobbies += h + ", "
+
+h = input("What else do you like? -->")
+hobbies += h + ", "
+
+h = input("Anything else? -->")
+hobbies += h + ", "
+
+print("So I love doing", hobbies)
