d81238a3584e13ba6b30f3d56a109a79db821a44
DumbFace303 <markbabasa131@gmail.com>
2026-09-08 12:14:51 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,18 @@
+#and = &&
+#or = !!
+#not = !
+
+
+a = 10
+b = 8
+c = 13
+
+#sequence
+#not, and, or
+
+print(a > b or c < a)
+
+print (a > b or c < a and c == a)
+
+print (not(a > b or c < a and c == a))
+
