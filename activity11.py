75ccf9b35d85e32b4e6942b83db8c55cbaf3ad6b
DumbFace303 <markbabasa131@gmail.com>
2026-09-08 12:32:18 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,13 @@
+#Conditional Statement
+
+username = "user1"
+password = "password123"
+
+u = input("Input Username = ")
+p = input("Input Password = ")
+
+if username == u and password == p :
+	print('"ACCESS GRANTED"')
+else : 
+	print('"ACCESS DENIED"')
+
