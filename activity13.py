f48be85d2f2ac4627f7dd56bc1219c98dbd96b1c
DumbFace303 <markbabasa131@gmail.com>
2026-09-10 12:18:51 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,28 @@
+
+
+
+age = int(input("Input Age = "))
+
+if age >= 1 and age <=5:
+	print("Infant")
+
+elif age >= 6 and age <= 12:
+	print("Kid")
+
+elif age >= 13  and age <= 19 :
+	print("Teenager")
+
+elif age >= 20 and age <= 29:
+	print("Early Adulthood")
+
+elif age >= 30 and age <= 45:
+	print("Adult")
+
+elif age >= 46 and age <= 59 :
+	print("Advance Adulthood")
+
+elif age >= 60 and age <= 150:
+	print("Senior")
+
+else:
+	print("Invalid")
