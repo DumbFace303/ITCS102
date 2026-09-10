7786e5cce68667d28dc9ce564ea23c9d372b0aad
DumbFace303 <markbabasa131@gmail.com>
2026-09-10 19:52:02 +0800

    Update CodeChallenge3.py

--- CodeChallenge3.py
+++ CodeChallenge3.py
@@ -2,37 +2,63 @@ sender = input("What is your name? = ")
 
 typeOfItem = input("What type of item? = ")
 
-isFragile = bool(input("Is your item fragile? = "))
-if isFragile == True:
-	print("Positive")
-else:
-	print("Negative")
+isFragile = bool(input('Is your item fragile? (Yes/No) =') == 'Yes')
 
 weight = float(input("What is the weight(KG) of your item? = "))
 
 distance = float(input("How far is the destination(KM)? = "))
 
-isExpress = bool(input("Is your delivery a priority? = "))
+isExpress = bool(input("Is your delivery a priority? (Yes/No) = ") == 'Yes')
+
+isInternational = bool(input("Is the destination international? (Yes/No)= ")=='Yes')
+
+
+
+if isFragile == True:
+	print('Fragile = Yes')
+else:
+	print('Fragile = No')
+	
 if isExpress == True:
-	print("Positive")
+	print("Priority = Yes")
 else:
-	print("Negative")
-
-isInternational = bool(input("Is the destination international? = "))
+	print('Priority = No')
+	
 if isInternational == True:
-	print("Positive")
+	print("International Delivery = Yes")
 else:
-	print("Negative")
+	print("International Delivery = No")
+	
 
 
+baseCost = (weight * 2.50) + (distance * 0.15)
 
-baseCost = (weight * 2.50)+(distance * 0.15)
 
-internationalExpress = total
-	total = (baseCost*1.40)+50
+if weight <= 2.0 and distance <= 100 and not isExpress and not isInternational:
+	Total = 0.00
 
-heavyInternational = total2
-	total2 = (baseCost*1.20)+25
+elif isInternational == True and isExpress == True:
+	Total = (baseCost * 1.40) + 50
 
-overSized = total3
-	total3 = baseCost+30
+elif isExpress == True or (isInternational == True and weight > 20):
+	Total = (baseCost * 1.20) + 25
+
+elif weight > 30 or distance > 1000:
+	Total = baseCost + 30
+
+else:
+	Total = baseCost
+
+shippingFee = Total - baseCost
+
+
+print('--------------------------------------------------------')
+print('Sender = ', sender)
+print('Fragile = ', typeOfItem)
+print('Weight = ', weight)
+print('Distance = ', distance)
+print('Priority = ', isExpress)
+print('International = ', isInternational)
+print('Base Cost = ', baseCost)
+print('Shipping Fee =', shippingFee)
+print('Total =', Total)
